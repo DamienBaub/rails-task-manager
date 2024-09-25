278e15b68e041eb09a620c5bd65fea9958707f5e
Damien Baubriau <dam.baubriau@gmail.com>
2024-09-25 08:27:40 +0100

    style

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
-  # All tasks
-  get "tasks", to: "tasks#index"
-  # Add task
-  get "tasks/new", to: "tasks#new"
-  post "tasks", to: "tasks#create"
-  # One task
-  get "tasks/:id", to: "tasks#show", as: :task
-  # Edit task
-  get "tasks/:id/edit", to: "tasks#edit", as: :edit
-  patch "tasks/:id", to: "tasks#update"
-  # Delete task
-  delete "tasks/:id", to: "tasks#destroy"
+  # get 'tasks', to: 'tasks#index'
+  # post 'tasks', to: 'tasks#create'
+  # get 'tasks/new', to: 'tasks#new', as: :new_task
+  # get 'tasks/:id', to: 'tasks#show', as: :task
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  # patch 'tasks/:id', to: 'tasks#update'
+  # delete 'tasks/:id', to: 'tasks#destroy'
+
+  resources :tasks
 end
